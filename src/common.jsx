c4c800c9391af8b95d0820027085bdc42fc1c916
quangdatcqd <quangdatcqd@gmail.com>
2023-03-14 18:20:09 +0700

    L

--- src/common.jsx
+++ src/common.jsx
@@ -59,19 +59,19 @@ export const generateID = () => {
         }
         return mac.toUpperCase();
     }
-    var XM1 = ' 1:__' + randomString(chars, 36) +
+    var XM1 = ' 1:__' + randomString(chars, 37) +
         ',2:asdfsdf,3:' + new Date().getTime() +
         '-7026432932894132169,4:52251,5:' + randomString(fullchars, 8) +
         '|19eew00|8,6:' + randomMAC() +
-        ',7:"' + randomString(fullchars, 15) +
-        '",8:720x1356,9:asdf,10:2,11:' + randomString(fullchars, 32) +
+        ',7:"' + randomString(fullchars, 13) +
+        '",8:720x1356,9:asdf,10:2,11:' + randomString(fullchars, 31) +
         'AAAAAAAAAAA=,12:' + randomString(chars, 40) +
         ',13:33005,14:7026432932894132169';
     localStorage.setItem("XM1", XM1);
 
-    localStorage.setItem("device_id", randomString(fullchars + "+/", 325) + "==")
+    localStorage.setItem("device_id", randomString(fullchars + "+/", 345) + "==")
 
-    localStorage.setItem("unique_id", randomString(chars, 33));
+    localStorage.setItem("unique_id", randomString(chars, 40));
     var tokenDevice = randomString(fullchars, 18) + "-DHZ:" + randomString(fullchars, 163);
     localStorage.setItem("token_device", tokenDevice);
 
